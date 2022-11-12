6b380ccfcf4c2f651f312701837c537aa3ba3dac
Lautaro Barba <lau_mb94@hotmail.com>
2022-11-11 22:10:47 -0300

    Primeras pruebas con leaflet

--- landingpage/public/assets/js/index.js
+++ landingpage/public/assets/js/index.js
@@ -2,3 +2,15 @@
 $(".nav-link").on("click", function () {
   $("#navbarLinks").collapse("hide");
 });
+
+// Leaflet
+var map = L.map('map').setView([-54.781111, -68.292613], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  minZoom: 12,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var marker = L.marker([-54.781111, -68.292613]).addTo(map);
+marker.bindPopup("Jardín Botánico Ushuaia").openPopup();
